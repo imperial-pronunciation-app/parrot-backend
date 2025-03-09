af74104f15dacaa6bd9f2cc98d1826a748475112
Henry Yu <henrytsyu@gmail.com>
2025-03-09 01:48:40 +0000

    fix: generate wotd for each language

--- app/services/word_of_day.py
+++ app/services/word_of_day.py
@@ -7,9 +7,12 @@ class WordOfDayService:
         self._uow = uow
 
     def change_word_of_day(self) -> None:
-        word = self._uow.words.get_word_not_used_for(days=365)
-        self._uow.word_of_day.add_word_of_day(word_id=word.id)
-        self._uow.words.update_date_of_word_last_used(word=word)
+        languages = self._uow.languages.all()
+        for language in languages:
+            word = self._uow.words.get_word_not_used_for(days=365, language=language)
+            self._uow.word_of_day.add_word_of_day(word_id=word.id)
+            self._uow.words.update_date_of_word_last_used(word=word)
+        self._uow.commit()
 
     def get_xp_gain(self, word_of_day: WordOfDay, user_id: int, score: int) -> int:
         max_score = self._uow.word_of_day_attempts.max_score_for_word_of_day(
