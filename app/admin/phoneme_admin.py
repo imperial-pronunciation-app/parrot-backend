27a247bc63982e03338b772d8721b275e866f54a
Henry Yu <henrytsyu@gmail.com>
2025-03-09 19:31:33 +0000

    fix: admin sortable columns

--- app/admin/phoneme_admin.py
+++ app/admin/phoneme_admin.py
@@ -6,4 +6,4 @@ from app.models import Phoneme
 class PhonemeAdmin(ModelView, model=Phoneme): # type: ignore[call-arg]
     column_list = [Phoneme.id, Phoneme.ipa, Phoneme.cdn_path, Phoneme.respellings, Phoneme.words]
     column_searchable_list = [Phoneme.ipa, Phoneme.cdn_path]
-    column_sortable_list = [Phoneme.id, Phoneme.ipa]
+    column_sortable_list = [Phoneme.id, Phoneme.ipa, Phoneme.cdn_path]
